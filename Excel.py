3e7d100036ceafe54e074b1a4e072dff4a086638
IsabelMarleen <i.leeni@t-online.de>
2020-05-30 23:31:50 +0200

    Started manipulating dataframe

--- Excel.py
+++ Excel.py
@@ -6,7 +6,47 @@ Created on Sun May 24 18:33:56 2020
 @author: isapoetzsch
 """
 
-#Read in Excel file
+#Setuo
 import pandas as pd
-df = pd.read_excel (r'~/Downloads/Darpa Template.xlsx')
+import os
+import sbol
+from sbol import Document
+from sbol import *
+
+#Read in Excel file
+
+path = '~/Downloads/Darpa Template.xlsx'
+df = pd.read_excel (path)
 print(df)
+
+df[df.columns[0:6]]
+basic_DNA_parts = df[12:37]
+
+
+#Create SBOL document
+doc = Document()
+
+#Define SBOL object and components
+GFP = ComponentDefinition('GFP', BIOPAX_DNA)
+doc.addComponentDefinition(GFP)
+       
+sequence =  """atgcgtaaag gagaagaact tttcactgga gttgtcccaa ttcttgttga
+            attagatggt gatgttaatg ggcacaaatt ttctgtcagt ggagagggtg
+            aaggtgatgc aacatacgga aaacttaccc ttaaatttat ttgcactact
+            ggaaaactac ctgttccatg gccaacactt gtcactactt tcggttatgg
+            tgttcaatgc tttgcgagat acccagatca tatgaaacag catgactttt
+            tcaagagtgc catgcccgaa ggttatgtac aggaaagaac tatatttttc
+            aaagatgacg ggaactacaa gacacgtgct gaagtcaagt ttgaaggtga
+            tacccttgtt aatagaatcg agttaaaagg tattgatttt aaagaagatg
+            gaaacattct tggacacaaa ttggaataca actataactc acacaatgta
+            tacatcatgg cagacaaaca aaagaatgga atcaaagtta acttcaaaat
+            tagacacaac attgaagatg gaagcgttca actagcagac cattatcaac
+            aaaatactcc aattggcgat ggccctgtcc ttttaccaga caaccattac
+            ctgtccacac aatctgccct ttcgaaagat cccaacgaaa agagagacca
+            catggtcctt cttgagtttg taacagctgc tgggattaca catggcatgg
+            atgaactata caaataataa"""
+GFP_seq = Sequence('GFP_seq', sequence, SBOL_ENCODING_IUPAC)
+doc.addSequence(GFP_seq)
+
+#Export
+doc.write('SBOL_example.xml')

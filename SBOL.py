41b6417fcfd0d1b6cd30794379c325c39f1b32ee
IsabelMarleen <i.leeni@t-online.de>
2020-05-30 18:09:18 +0200

    Added sequence

--- SBOL.py
+++ SBOL.py
@@ -1,16 +1,33 @@
-# -*- coding: utf-8 -*-
-
-
+#Setup
 import sbol
+from sbol import Document
 from sbol import *
+
+#Create SBOL document
 doc = Document()
 
-setHomespace('http://sbols.org/CRISPR_Example')
+#Define SBOL object and components
+GFP = ComponentDefinition('GFP', BIOPAX_DNA)
+doc.addComponentDefinition(GFP)
+       
+sequence =  """atgcgtaaag gagaagaact tttcactgga gttgtcccaa ttcttgttga
+            attagatggt gatgttaatg ggcacaaatt ttctgtcagt ggagagggtg
+            aaggtgatgc aacatacgga aaacttaccc ttaaatttat ttgcactact
+            ggaaaactac ctgttccatg gccaacactt gtcactactt tcggttatgg
+            tgttcaatgc tttgcgagat acccagatca tatgaaacag catgactttt
+            tcaagagtgc catgcccgaa ggttatgtac aggaaagaac tatatttttc
+            aaagatgacg ggaactacaa gacacgtgct gaagtcaagt ttgaaggtga
+            tacccttgtt aatagaatcg agttaaaagg tattgatttt aaagaagatg
+            gaaacattct tggacacaaa ttggaataca actataactc acacaatgta
+            tacatcatgg cagacaaaca aaagaatgga atcaaagtta acttcaaaat
+            tagacacaac attgaagatg gaagcgttca actagcagac cattatcaac
+            aaaatactcc aattggcgat ggccctgtcc ttttaccaga caaccattac
+            ctgtccacac aatctgccct ttcgaaagat cccaacgaaa agagagacca
+            catggtcctt cttgagtttg taacagctgc tgggattaca catggcatgg
+            atgaactata caaataataa"""
 
-cas9_generic = ComponentDefinition('cas9_generic', BIOPAX_PROTEIN)
-doc.addComponentDefinition(cas9_generic)
+GFP_seq = Sequence('GFP_seq', sequence, SBOL_ENCODING_IUPAC)
+doc.addSequence(GFP_seq)
 
-cd0 = ComponentDefinition('cd0')
-doc.addComponentDefinition(cd0)
-
+#Export
 doc.write('SBOL_example.xml')

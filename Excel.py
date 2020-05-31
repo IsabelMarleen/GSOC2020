61e226eb0f92bdfb4ebe45d16fa3d6d3ae47a3ec
IsabelMarleen <i.leeni@t-online.de>
2020-05-31 17:16:04 +0200

    First attempt for loop to create components, still erroneous

--- Excel.py
+++ Excel.py
@@ -25,36 +25,19 @@ basic_DNA_parts = df[12:37]
 #Reassign index and column names
 basic_DNA_parts.reset_index(drop=True, inplace=True)
 basic_DNA_parts.columns = basic_DNA_parts.iloc[0]
+basic_DNA_parts = basic_DNA_parts.drop([0])
+#Line to remove space in part name strings to avoid error when using that name 
+#when defining components
+basic_DNA_parts['Part Name'].apply(basic_DNA_parts.replace( [' '], ['_']))
 
 #Create SBOL document
 doc = Document()
 
 #Define SBOL object and components
-GFP = ComponentDefinition('GFP', BIOPAX_DNA)
-doc.addComponentDefinition(GFP)
+#Not yet functional because blanks in Part Name column
+molecule_type = BIOPAX_DNA
 
-       
-sequence =  """atgcgtaaag gagaagaact tttcactgga gttgtcccaa ttcttgttga
-            attagatggt gatgttaatg ggcacaaatt ttctgtcagt ggagagggtg
-            aaggtgatgc aacatacgga aaacttaccc ttaaatttat ttgcactact
-            ggaaaactac ctgttccatg gccaacactt gtcactactt tcggttatgg
-            tgttcaatgc tttgcgagat acccagatca tatgaaacag catgactttt
-            tcaagagtgc catgcccgaa ggttatgtac aggaaagaac tatatttttc
-            aaagatgacg ggaactacaa gacacgtgct gaagtcaagt ttgaaggtga
-            tacccttgtt aatagaatcg agttaaaagg tattgatttt aaagaagatg
-            gaaacattct tggacacaaa ttggaataca actataactc acacaatgta
-            tacatcatgg cagacaaaca aaagaatgga atcaaagtta acttcaaaat
-            tagacacaac attgaagatg gaagcgttca actagcagac cattatcaac
-            aaaatactcc aattggcgat ggccctgtcc ttttaccaga caaccattac
-            ctgtccacac aatctgccct ttcgaaagat cccaacgaaa agagagacca
-            catggtcctt cttgagtttg taacagctgc tgggattaca catggcatgg
-            atgaactata caaataataa"""
-GFP_seq = Sequence('GFP_seq', sequence, SBOL_ENCODING_IUPAC)
-doc.addSequence(GFP_seq)
-
-#Export
-doc.write('SBOL_example.xml')
-
-basic_DNA_parts.renameindex={1: 'a'})
+for component in basic_DNA_parts['Part Name']: 
+   component = ComponentDefinition(component, molecule_type)
 
 

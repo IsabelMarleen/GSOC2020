ae64d613c2af45f58a3e6914b7d34826064a1ef6
JMante1 <40006518+JMante1@users.noreply.github.com>
2020-06-08 18:57:26 -0700

    Have added a few more comments
    Added a few comments to make things clearer.

--- Excel.py
+++ Excel.py
@@ -22,29 +22,42 @@ path_filled = os.path.join(cwd, "darpa_template.xlsx")
 
 
 #Read in template and filled spreadsheet
-def read_spreadsheet( path, start_row, nrows, use_cols ):
+def read_spreadsheet(path, start_row, nrows, use_cols):
     """
-    the function reads and formats an excel spreadsheet
+    the function reads and formats an excel spreadsheet based on the Darpa template.
+    It requires a sheet with the name "Library".
+    
+    Requires
+    --------
+    import pandas as pd
 
     Parameters
     ----------
     path : STRING
         Path to Excel Spreadsheet
-    start_row : integer
-        Defines first row to be read
+    start_row : INTGER
+        Defines first row to be read for the parts table
     nrows: INTEGER
-        Defines number of rows to be read
-    usecols: INTEGER
-        Defines which columns should be read
-        
-    #Add to here
+        Defines number of rows to be read for the metadata section
+    usecols: LIST
+        Defines which columns should be read for the metadata section (note column A is 0)
 
     Returns
     -------
-    pandas dataframe
-        DESCRIPTION.
+    basic_DNA_parts: DATAFRAME
+        The parts table with headers from row=start_row and data from all rows after that.
+    metadata: DATAFRAME, (usecols x nrows)
+        A header less table of length nrows and width usecols
+    
+    Example
+    -------
+    cwd = os.path.dirname(os.path.abspath("__file__")) #get current working directory
+    path_filled = os.path.join(cwd, "darpa_template.xlsx")
+    filled_data, filled_metadata = readspreadsheet(path_filled,  
+                start_row = 13, nrows = 8, use_cols = [0,1])
 
     """
+    #add error catching if the sheet Library doesn't exist? Should Library be a variable and not hardcoded?
     basic_DNA_parts = pd.read_excel (path, sheet_name = "Library",
                                   header= 0, skiprows = start_row)
     
@@ -70,7 +83,7 @@ filled_columns = set(filled_data.columns)
 blank_columns = set(blank_data.columns)
 
 if not(blank_columns.issubset(filled_columns)) :
-    logging.warning("Required columns are missing")
+    logging.warning("Some of the required columns are missing")
 
 
 #Create SBOL document

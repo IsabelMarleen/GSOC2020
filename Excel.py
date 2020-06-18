098aab869b99c82a0bf0d287af451ec6f2fe8e74
IsabelMarleen <i.leeni@t-online.de>
2020-06-18 12:28:55 +0200

    Minor changes

--- Excel.py
+++ Excel.py
@@ -11,42 +11,43 @@ import pandas as pd
 import numpy as np
 import os
 import logging
+import col_to_excel
+from col_to_excel import col_to_excel
 import sbol2
 from sbol2 import Document, Component, ComponentDefinition
 from sbol2 import BIOPAX_DNA, Sequence, SBOL_ENCODING_IUPAC
 
 
-def col_to_excel(col):
-    """
-    Converts the column number to the excel column name (A, B, ... AA  etc)
+# def col_to_excel(col):
+#     """
+#     Converts the column number to the excel column name (A, B, ... AA  etc)
 
-    Parameters
-    ----------
-    col : INTEGER
-        The number of the column to convert. Note that 1 converts to A
+#     Parameters
+#     ----------
+#     col : INTEGER
+#         The number of the column to convert. Note that 1 converts to A
 
-    Returns
-    -------
-    excel_col : STRING
-        The string which describes the name of the column in Excel
+#     Returns
+#     -------
+#     excel_col : STRING
+#         The string which describes the name of the column in Excel
         
-    Example
-    -------
-    print(col_to_excel(9))
+#     Example
+#     -------
+#     print(col_to_excel(9))
 
-    """
-    excel_col = ""
-    div = col 
+#     """
+#     excel_col = ""
+#     div = col 
     
-    while div>0:
-        (div, mod) = divmod(div-1, 26) # will return (x, 0 .. 25)
-        excel_col = chr(mod + 65) + excel_col
+#     while div>0:
+#         (div, mod) = divmod(div-1, 26) # will return (x, 0 .. 25)
+#         excel_col = chr(mod + 65) + excel_col
 
-    return excel_col
+#     return excel_col
 
 
 cwd = os.path.dirname(os.path.abspath("__file__")) #get current working directory
-cwd = os.path.join(cwd, "Documents", "Utah 2020", "GSoC", "GSOC2020")
 path_blank = os.path.join(cwd, "darpa_template_blank.xlsx")
 path_filled = os.path.join(cwd, "darpa_template.xlsx")
 

d8b6ba88060f0699c4d80ae1193093ff157d1cbf
IsabelMarleen <i.leeni@t-online.de>
2020-07-06 15:41:59 +0200

    Composite Part Examples

--- SBOL.py
+++ SBOL.py
@@ -29,5 +29,51 @@ sequence =  """atgcgtaaag gagaagaact tttcactgga gttgtcccaa ttcttgttga
 GFP_seq = Sequence('GFP_seq', sequence, SBOL_ENCODING_IUPAC)
 doc.addSequence(GFP_seq)
 
+LacY = ComponentDefinition('LacY', BIOPAX_DNA)
+doc.addComponentDefinition(LacY)
+       
+sequence2 =  """﻿atgtctgccc gtatttcgcg taaggaaatc cattatgtac tatttaaaaa
+            acacaaactt ttggatgttc ggtttattct ttttctttta cttttttatc
+            atgggagcct acttcccgtt tttcccgatt tggctacatg acatcaacca
+            tatcagcaaa agtgatacgg gtattatttt tgccgctatt tctctgttct
+            cgctattatt ccaaccgctg tttggtctgc tttctgacaa actcgggctg
+            cgcaaatacc tgctgtggat tattaccggc atgttagtga tgtttgcgcc
+            gttctttatt tttatcttcg ggccactgtt acaatacaac attttagtag
+            gatcgattgt tggtggtatt tatctaggct tttgttttaa cgccggtgcg
+            ccagcagtag aggcatttat tgagaaagtc agccgtcgca gtaatttcga
+            atttggtcgc gcgcggatgt ttggctgtgt tggctgggcg ctgtgtgcct
+            cgattgtcgg catcatgttc accatcaata atcagtttgt tttctggctg
+            ggctctggct gtgcactcat cctcgccgtt ttactctttt tcgccaaaac
+            ggatgcgccc tcttctgcca cggttgccaa tgcggtaggt gccaaccatt
+            cggcatttag ccttaagctg gcactggaac tgttcagaca gccaaaactg
+            tggtttttgt cactgtatgt tattggcgtt tcctgcacct acgatgtttt
+            tgaccaacag tttgctaatt tctttacttc gttctttgct accggtgaac
+            agggtacgcg ggtatttggc tacgtaacga caatgggcga attacttaac
+            gcctcgatta tgttctttgc gccactgatc attaatcgca tcggtgggaa
+            aaacgccctg ctgctggctg gcactattat gtctgtacgt attattggct
+            catcgttcgc cacctcagcg ctggaagtgg ttattctgaa aacgctgcat
+            atgtttgaag taccgttcct gctggtgggc tgctttaaat atattaccag
+            ccagtttgaa gtgcgttttt cagcgacgat ttatctggtc tgtttctgct
+            tctttaagca actggcgatg atttttatgt ctgtactggc gggcaatatg
+            tatgaaagca tcggtttcca gggcgcttat ctggtgctgg gtctggtggc
+            gctgggcttc accttaattt ccgtgttcac gcttagcggc cccggcccgc
+            tttccctgct gcgtcgtcag gtgaatgaag tcgcttaa"""
+
+LacY_seq = Sequence('Lac_seq', sequence2, SBOL_ENCODING_IUPAC)
+doc.addSequence(LacY_seq)
+
+#Composition
+composition_component = doc.componentDefinitions.create("composition_component")
+composition_component.assemblePrimaryStructure([GFP, LacY])
+for cd in composition_component.getPrimaryStructure():
+    print(cd.displayId)
+    
+nucleotides = composition_component.compile()
+# print (nucleotides)
+# seq = composition_component.sequence
+# print(seq.elements)
+
+composition_component.roles = [SO_GENE]
+
 #Export
 doc.write('SBOL_example.xml')
